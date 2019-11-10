a1f791971a4f2a4174bec5764953c898c978a95d
Giulia Cantini <giuliacantini3@gmail.com>
2019-11-10 19:27:27 +0100

    Add .ini file for testing with configobj

--- fc_graphobs/test.py
+++ fc_graphobs/test.py
@@ -18,10 +18,12 @@ from fc_graphobs.print_info import print_info
 import fc_graphobs.nets
 import time
 
+from configobj import ConfigObj
+
 base_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(base_dir))
 
-
+'''
 class RandomAgent:
 
     def __init__(self, state_size, action_size):
@@ -42,6 +44,12 @@ class RandomAgent:
         # Load a policy
         return
 
+'''
+
+
+config = ConfigObj("./tests-config.ini")
+tests = config.sections
+n_tests = len(tests)
 
 width = 60
 height = 60
@@ -53,12 +61,6 @@ max_rails_between_cities = 2  # Max number of tracks allowed between cities. Thi
 max_rail_in_cities = 3  # Max number of parallel tracks within a city, representing a realistic train station
 
 
-rail_generator = sparse_rail_generator(max_num_cities=cities_in_map,
-                                       seed=seed,
-                                       grid_mode=grid_distribution_of_cities,
-                                       max_rails_between_cities=max_rails_between_cities,
-                                       max_rails_in_city=max_rail_in_cities,
-                                       )
 
 
 # rail_generator = rail_from_file("../test-envs/Test_0/Level_0.pkl")
@@ -71,32 +73,11 @@ speed_ration_map = {1.: 0.25,  # Fast passenger train
 
 schedule_generator = sparse_schedule_generator(speed_ration_map)
 
-stochastic_data = {
-    'malfunction_rate': 10000,  # Rate of malfunction occurrence of single agent
-    'min_duration': 15,  # Minimal duration of malfunction
-    'max_duration': 50  # Max duration of malfunction
-    }
 
-prediction_depth = 30
+
+prediction_depth = 40
 observation_builder = GraphObsForRailEnv(bfs_depth=4, predictor=ShortestPathPredictorForRailEnv(max_depth=prediction_depth))
 
-# Construct the environment with the given observation, generators, predictors, and stochastic data
-env = RailEnv(width=width,
-              height=height,
-              rail_generator=rail_generator,
-              schedule_generator=schedule_generator,
-              number_of_agents=nr_trains,
-              obs_builder_object=observation_builder,
-              malfunction_generator_and_process_data=malfunction_from_params(stochastic_data),
-              remove_agents_at_target=True)
-env.reset()
-
-# Initiate the renderer
-env_renderer = RenderTool(env, gl="PILSVG",
-                          agent_render_variant=AgentRenderVariant.AGENT_SHOWS_OPTIONS_AND_BOX,
-                          show_debug=True,
-                          screen_height=1080,  # Adjust these parameters to fit your resolution
-                          screen_width=1920)  # Adjust these parameters to fit your resolution
 
 state_size = prediction_depth + 3
 network_action_size = 2
@@ -105,49 +86,80 @@ controller = Agent(state_size, network_action_size)
 network_action_dict = dict()
 railenv_action_dict = dict()
 
-observations, infos = env.reset(True, True)
-env_renderer.reset()
-
 score = 0
 # Run episode
 frame_step = 0
 
 # Here you can pre-load an agent
-with path(fc_graphobs.nets, "avoid_checkpoint20.pth") as file_in:
+with path(fc_graphobs.nets, "avoid_checkpoint60.pth") as file_in:
     controller.qnetwork_local.load_state_dict(torch.load(file_in))
 
-for step in range(200):
+for test in tests:
 
-    print_info(env)
-    # Chose an action for each agent in the environment
-    for a in range(env.get_num_agents()):
-        shortest_path_action = int(observation_builder.get_shortest_path_action(a))
-        # 'railenv_action' is in [0, 4], network_action' is in [0, 1]
-        # 'network_action' is None if act() returned a random sampled action
-        railenv_action, network_action = controller.act(observations[a], shortest_path_action)
-        railenv_action_dict.update({a: railenv_action})
-        network_action_dict.update({a: network_action})
+    # Build the env according to config parameters
+    env = RailEnv(width=config[test].as_int('width'),
+                  height=config[test].as_int('height'),
+                  rail_generator=sparse_rail_generator(
+                            max_num_cities=config[test].as_int('n_cities'), 
+                            seed=seed,
+                            grid_mode=grid_distribution_of_cities,
+                            max_rails_between_cities=config[test].as_int('max_rails_between_cities'),
+                            max_rails_in_city=config[test].as_int('max_rail_in_city')
+                  ),
+                  schedule_generator=schedule_generator,
+                  number_of_agents=nr_trains,
+                  obs_builder_object=observation_builder,
+                  malfunction_generator_and_process_data=malfunction_from_params(
+                      parameters={
+                        'malfunction_rate': config[test].as_int('malfunction_rate'),  # Rate of malfunction occurrence of single agent
+                        'min_duration': config[test].as_int('min_duration'),  # Minimal duration of malfunction
+                        'max_duration': config[test].as_int('max_duration')  # Max duration of malfunction
+                        }),
+                  remove_agents_at_target=True)
 
-    # Environment step which returns the observations for all agents, their corresponding
-    # reward and whether their are done
+    # Initiate the renderer # TODO Inizializzare il render_env una sola volta
+    env_renderer = RenderTool(env, gl="PILSVG",
+                              agent_render_variant=AgentRenderVariant.AGENT_SHOWS_OPTIONS_AND_BOX,
+                              show_debug=True,
+                              screen_height=1080,  # Adjust these parameters to fit your resolution
+                              screen_width=1920)  # Adjust these parameters to fit your resolution
 
-    next_obs, all_rewards, done, _ = env.step(railenv_action_dict)
+    observations, infos = env.reset(True, True)
+    env_renderer.reset()
+    max_time_steps = int(4 * 2 * (config[test].as_int('width') + config[test].as_int('height') + config[test].as_int('n_agents') / config[test].as_int('n_cities')))
 
-    # Which agents needs to pick and action
-    print("\n The following agents can register an action:")
-    print("========================================")
-    for info in infos['action_required']:
-        print("Agent {} needs to submit an action.".format(info))
-
-    env_renderer.render_env(show=True, show_observations=False, show_predictions=False)
-    frame_step += 1
-    # Update replay buffer and train agent
-    for a in range(env.get_num_agents()):
-        controller.step(observations[a], network_action_dict[a], all_rewards[a], next_obs[a], done[a])
-        score += all_rewards[a]
-
-    observations = next_obs.copy()
-    if done['__all__']:
-        break
-
-    print('Episode: Steps {}\t Score = {}'.format(step, score))
+    for step in range(max_time_steps):
+    
+        print_info(env)
+        # Chose an action for each agent in the environment
+        for a in range(env.get_num_agents()):
+            shortest_path_action = int(observation_builder.get_shortest_path_action(a))
+            # 'railenv_action' is in [0, 4], network_action' is in [0, 1]
+            # 'network_action' is None if act() returned a random sampled action
+            railenv_action, network_action = controller.act(observations[a], shortest_path_action)
+            railenv_action_dict.update({a: railenv_action})
+            network_action_dict.update({a: network_action})
+    
+        # Environment step which returns the observations for all agents, their corresponding
+        # reward and whether their are done
+    
+        next_obs, all_rewards, done, _ = env.step(railenv_action_dict)
+    
+        # Which agents needs to pick and action
+        print("\n The following agents can register an action:")
+        print("========================================")
+        for info in infos['action_required']:
+            print("Agent {} needs to submit an action.".format(info))
+    
+        env_renderer.render_env(show=True, show_observations=False, show_predictions=False)
+        frame_step += 1
+        # Update replay buffer and train agent
+        for a in range(env.get_num_agents()):
+            controller.step(observations[a], network_action_dict[a], all_rewards[a], next_obs[a], done[a])
+            score += all_rewards[a]
+    
+        observations = next_obs.copy()
+        if done['__all__']:
+            break
+    
+        print('Episode: Steps {}\t Score = {}'.format(step, score))
