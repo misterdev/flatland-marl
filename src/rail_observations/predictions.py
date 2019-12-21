f055ebe97cdac79d6c16bdc7ccf79efc710aa475
Giulia Cantini <giuliacantini3@gmail.com>
2019-12-21 16:20:35 +0100

    Add rail observations

---
+++ src/rail_observations/predictions.py
@@ -0,0 +1,158 @@
+"""
+Collection of environment-specific PredictionBuilder.
+"""
+
+import numpy as np
+from collections import defaultdict
+from typing import NamedTuple, Tuple, List
+
+
+from flatland.core.env_prediction_builder import PredictionBuilder
+from flatland.envs.agent_utils import RailAgentStatus
+from flatland.envs.rail_env import RailEnv
+from flatland.envs.distance_map import DistanceMap
+from flatland.envs.rail_env import RailEnvActions, RailEnvNextAction
+from flatland.envs.rail_env_shortest_paths import get_shortest_paths, get_new_position
+from flatland.utils.ordered_set import OrderedSet
+
+
+
+WalkingElement = \
+    NamedTuple('WalkingElement',
+               [('position', Tuple[int, int]), ('direction', int), ('next_action_element', RailEnvNextAction)])
+
+# Flatland v3
+# A way point is the entry into a cell defined by
+# - the row and column coordinates of the cell entered
+# - direction, in which the agent is facing to enter the cell.
+# This induces a graph on top of the FLATland cells:
+# - four possible way points per cell
+# - edges are the possible transitions in the cell.
+Waypoint = NamedTuple('Waypoint', [('position', Tuple[int, int]), ('direction', int)])
+
+
+# TODO 'Add action taken to come here' info
+
+class ShortestPathPredictorForRailEnv(PredictionBuilder):
+    """
+    ShortestPathPredictorForRailEnv object.
+
+    This object returns shortest-path predictions for agents in the RailEnv environment.
+    The prediction acts as if no other agent is in the environment and always takes the forward action.
+    """
+
+    def __init__(self, max_depth: int = 20):
+        super().__init__(max_depth)
+
+    def get(self, handle: int = None):
+        """
+        Requires distance_map to extract the shortest path.
+        Does not take into account future positions of other agents!
+
+        If there is no shortest path, the agent just stands still and stops moving.
+
+        Parameters
+        ----------
+        handle : int, optional
+            Handle of the agent for which to compute the observation vector.
+
+        Returns
+        -------
+        np.array
+            Returns a dictionary indexed by the agent handle and for each agent a vector of (max_depth + 1)x5 elements:
+            - time_offset
+            - position axis 0
+            - position axis 1
+            - direction
+            - action taken to come here - must be implemented TODO
+            The prediction at 0 is the current position, direction etc.
+        """
+        agents = self.env.agents
+        if handle:
+            agents = [self.env.agents[handle]]
+        distance_map: DistanceMap = self.env.distance_map
+
+        shortest_paths = get_shortest_paths(distance_map, max_depth=self.max_depth + 1)
+        
+        prediction_dict = {}
+        for agent in agents:
+
+            if agent.status == RailAgentStatus.READY_TO_DEPART:
+                agent_virtual_position = agent.initial_position
+            elif agent.status == RailAgentStatus.ACTIVE:
+                agent_virtual_position = agent.position
+            elif agent.status == RailAgentStatus.DONE:
+                agent_virtual_position = agent.target
+            else:  # agent.status == DONE_REMOVED, prediction must be None
+                prediction = np.zeros(shape=(self.max_depth + 1, 5))
+                for i in range(self.max_depth + 1):
+                    prediction[i] = [i, None, None, None, None]
+                prediction_dict[agent.handle] = prediction
+                continue
+
+            agent_virtual_direction = agent.direction
+            agent_speed = agent.speed_data["speed"]
+            times_per_cell = int(np.reciprocal(agent_speed))
+            prediction = np.zeros(shape=(self.max_depth + 1, 5))
+            # First cell is info relative to actual timestep
+            prediction[0] = [0, *agent_virtual_position, agent_virtual_direction, 0]
+
+            shortest_path = shortest_paths[agent.handle]
+
+            # If there is a shortest path, remove the initial position
+            if shortest_path:
+                shortest_path = shortest_path[1:]
+
+            new_direction = agent_virtual_direction
+            new_position = agent_virtual_position
+            visited = OrderedSet()
+            for index in range(1, self.max_depth + 1):
+                # If we're at the target or not moving, stop moving until max_depth is reached
+                if new_position == agent.target or not agent.moving or not shortest_path:
+                #if new_position == agent.target or not shortest_path: # Writing like this you don't consider the fact that the agent is stopped
+                    prediction[index] = [index, *new_position, new_direction, RailEnvActions.STOP_MOVING]
+                    visited.add((*new_position, agent.direction))
+                    continue
+
+                if index % times_per_cell == 0:
+
+                    new_position = shortest_path[0].position
+                    new_direction = shortest_path[0].direction
+
+                    shortest_path = shortest_path[1:]
+
+                # Prediction is ready
+                prediction[index] = [index, *new_position, new_direction, 0]
+                visited.add((*new_position, new_direction))
+
+            # TODO: very bady side effects for visualization only: hand the dev_pred_dict back instead of setting on env!
+            self.env.dev_pred_dict[agent.handle] = visited
+            prediction_dict[agent.handle] = prediction
+
+        return prediction_dict
+
+    def compute_cells_sequence(self, prediction_dict):
+        """
+        Given prediction dict for all agents, return sequence of cells walked in the prediction as a dict
+        where key is the agent handle and value is the list of tuples (xi, yi) that are crossed.
+        Mostly used to debug.
+        :param prediction_dict: 
+        :return: 
+        """
+        
+
+        cells_sequence = defaultdict(list)
+        agents = self.env.agents
+        for a in agents:
+            for step in prediction_dict[a.handle]:
+                cell_pos = (step[1], step[2])  # Takes (yi, xi)
+                cells_sequence[a.handle].append(cell_pos)
+
+        return cells_sequence
+
+    def get_prediction_depth(self):
+        """
+        
+        :return: 
+        """
+        return self.max_depth
---
+++ src/rail_observations/rail_observations.py
@@ -0,0 +1,237 @@
+"""
+A class to build observations for agents.
+"""
+import collections
+from typing import Optional, List, Dict, Tuple, NamedTuple
+
+import numpy as np
+
+from flatland.core.env import Environment
+from flatland.core.env_observation_builder import ObservationBuilder
+from flatland.core.grid.grid4_utils import get_new_position
+
+CardinalNode = \
+	NamedTuple('CardinalNode', [('id_node', int), ('cardinal_point', int)])
+
+"""
+Things to figure out:
+- the observation acts considering that ALL agents start at once at the beginning, so at ts=0 they already have a position on the grid 
+Could be useful to have a limited number of agents contemporary active to control the number of bitmaps to feed to the network.
+- Now the prediction computes the path as if agents never stopped.
+- How to consider a switch: part of a rail, include it in the bitmap or not, etc. now there are rails connecting two switches 
+- The path to draw the bitmap must be recomputed at every ts? so maybe prediction depth so high is inefficient and also not necessary
+"""
+
+class RailObsForRailEnv(ObservationBuilder):
+	"""
+	This class returns an observation of rails occupancy in time in a bitmap.
+	# TODO Draw the bitmap
+	"""
+
+	def __init__(self, predictor):
+		"""
+	
+		:param predictor: class that predicts the path.
+		"""
+		super(RailObsForRailEnv, self).__init__()
+		self.predictor = predictor
+		self.prediction_dict = {}  # Dict handle : list of tuples representing prediction steps
+		self.cells_sequence = {} # Dict handle : list of tuples representing cell positions
+		
+		self.num_rails = None # Depends on the map, must be computed in reset()
+		self.max_time_steps = self.predictor.max_depth
+		# Not all of them are necessary
+		self.cell_to_id_node = {} # Map cell position : id_node
+		self.id_node_to_cell = {} # Map id_node to cell position
+		self.connections = {} # Map id_node : connections(node)
+		self.info = {} # Map id_edge : tuple (CardinalNode1, CardinalNode2, edge_length)
+		self.id_edge_to_cells = {} # Map id_edge : list of tuples (cell pos, crossing dir) in rail (nodes are not counted)
+		self.nodes = set() # Set of node ids
+		self.edges = set() # Set of edge ids
+
+	def set_env(self, env: Environment):
+		super().set_env(env)
+		if self.predictor:
+			# Use set_env available in PredictionBuilder (parent class)
+			self.predictor.set_env(self.env)
+
+	def reset(self):
+		"""
+		
+		:return: 
+		"""
+		self._map_to_graph() # Fill member variables
+		
+		
+	def get(self, handle: int = 0) -> np.ndarray:
+		"""
+		Currently this implementation of bitmap has got 'holes' of zeros when agent lies on a switch. TODO
+		:param handle: 
+		:return: Bitmap of rail obs for agent handle.
+		"""
+		
+		rail_obs = np.zeros((self.num_rails, self.max_time_steps + 1), dtype=int) # Max steps in the future + current ts
+		agent = self.env.agents[handle]
+		path = self.cells_sequence[handle]
+		# Truncate path in the future, after reaching target TODO Think
+		target_index = [i for i, pos in enumerate(path) if pos[0] == agent.target[0] and pos[1] == agent.target[1]]
+		if len(target_index) != 0:
+			target_index = target_index[0]
+			path = path[:target_index+1]
+		
+		for ts in range(self.max_time_steps + 1):
+			# Fill rail occupancy according to predicted position at ts
+			for cell in path:
+				# Find rail associated to cell
+				rail, dist = self.get_edge_from_cell(cell)
+				# Find crossing direction
+				if rail != -1: # Means agent is not on a switch
+					direction = self.id_edge_to_cells[rail][dist][1]
+					crossing_dir = 1 if direction == agent.direction else -1 # Direction saved is considered as crossing_dir = 1
+					
+					rail_obs[rail, ts] = crossing_dir
+		
+		return rail_obs
+		
+		
+	def get_many(self, handles: Optional[List[int]] = None) -> Dict[int, np.ndarray]:
+		"""
+		
+		:param handles: 
+		:return: 
+		"""
+		self.prediction_dict = self.predictor.get()
+		self.cells_sequence = self.predictor.compute_cells_sequence(self.prediction_dict)
+		
+		observations = {}
+		for a in handles:
+			observations[a] = self.get(a)
+		return observations
+	
+	# Slightly modified wrt to the other TODO Complete definition of rails
+	def _map_to_graph(self):
+		"""
+
+		:return: 
+		"""
+		id_node_counter = 0
+		# targets = [agent.target for agent in self.env.agents]
+
+		# Identify cells hat are nodes (have switches)
+		for i in range(self.env.height):
+			for j in range(self.env.width):
+
+				is_switch = False
+				is_crossing = False
+				# is_target = False
+				connections_matrix = np.zeros((4, 4))  # Matrix NESW x NESW
+
+				# Check if diamond crossing
+				transitions_bit = bin(self.env.rail.get_full_transitions(i, j))
+				if int(transitions_bit, 2) == int('1000010000100001', 2):
+					is_crossing = True
+					connections_matrix[0, 2] = connections_matrix[2, 0] = 1
+					connections_matrix[1, 3] = connections_matrix[3, 1] = 1
+
+				else:
+					# Check if target
+					# if (i, j) in targets:
+					#	is_target = True
+					# Check if switch
+					for direction in (0, 1, 2, 3):  # 0:N, 1:E, 2:S, 3:W
+						possible_transitions = self.env.rail.get_transitions(i, j, direction)
+						for t in range(4):  # Check groups of bits
+							if possible_transitions[t]:
+								inv_direction = (direction + 2) % 4
+								connections_matrix[inv_direction, t] = connections_matrix[t, inv_direction] = 1
+						num_transitions = np.count_nonzero(possible_transitions)
+						if num_transitions > 1:
+							is_switch = True
+
+				if is_switch or is_crossing: #or is_target:
+					# Add node - keep info on cell position
+					# Update only for nodes that are switches
+					self.connections.update({id_node_counter: connections_matrix})
+					self.id_node_to_cell.update({id_node_counter: (i, j)})
+					self.cell_to_id_node.update({(i, j): id_node_counter})
+					id_node_counter += 1
+
+		# Enumerate edges from these nodes
+		id_edge_counter = 0
+		# Start from connections of one node and follow path until next switch is found
+		nodes = self.connections.keys()  # ids
+		visited = set()  # Keeps set of CardinalNodes that were already visited
+		for n in nodes:
+			for cp in range(4):  # Check edges from the 4 cardinal points
+				if np.count_nonzero(self.connections[n][cp, :]) > 0:
+					visited.add(CardinalNode(n, cp))  # Add to visited
+					cells_sequence = []
+					node_found = False
+					edge_length = 0
+					# Keep going until another node is found
+					direction = cp
+					pos = self.id_node_to_cell[n]
+					while not node_found:
+						neighbour_pos = get_new_position(pos, direction)
+						cells_sequence.append((neighbour_pos, direction))
+						if neighbour_pos in self.cell_to_id_node:  # If neighbour is a node
+							# node_found = True
+							# Build edge, mark visited
+							id_node1 = n
+							cp1 = cp
+							id_node2 = self.cell_to_id_node[neighbour_pos]
+							cp2 = self._reverse_dir(direction)
+							if CardinalNode(id_node2, cp2) not in visited:
+								self.info.update({id_edge_counter:
+									                  (CardinalNode(id_node1, cp1),
+									                   CardinalNode(id_node2, cp2),
+									                   edge_length)})
+								cells_sequence.pop()  # Don't include this node in the edge
+								self.id_edge_to_cells.update({id_edge_counter: cells_sequence})
+								id_edge_counter += 1
+								visited.add(CardinalNode(id_node2, cp2))
+							break
+						edge_length += 1  # Not considering switches in the count
+						# Update pos and dir
+						pos = neighbour_pos
+						exit_dir = self._reverse_dir(direction)
+						possible_transitions = np.array(self.env.rail.get_transitions(pos[0], pos[1], direction))
+						possible_transitions[exit_dir] = 0  # Don't consider direction from which I entered
+						# t = 2
+						t = np.argmax(
+							possible_transitions)  # There's only one possible transition except the one that I took to get in
+						temp_pos = get_new_position(pos, t)
+						if 0 <= temp_pos[0] < self.env.height and 0 <= temp_pos[1] < self.env.width:  # Patch - check if this cell is a rail
+							# Entrance dir is always opposite to exit dir
+							direction = t
+
+		edges = self.info.keys()
+
+		self.nodes = nodes # Set of nodes
+		self.edges = self.info.keys() # Set of edges
+		self.num_rails = len(self.edges)
+
+	def get_edge_from_cell(self, cell):
+		"""
+
+		:param cell: Cell for which we want to find the associated rail id.
+		:return: A tuple (id rail, dist) where dist is the distance as offset from the beginning of the rail.
+		"""
+
+		dist = 0
+		for edge in self.id_edge_to_cells.keys():
+			if cell in [cell[0] for cell in self.id_edge_to_cells[edge]]:
+				return edge, dist
+			dist += 1
+
+		return -1, -1  # Node
+
+	@staticmethod
+	def _reverse_dir(direction):
+		"""
+		Invert direction (int) of one agent.
+		:param direction: 
+		:return: 
+		"""
+		return int((direction + 2) % 4)
+		pass
